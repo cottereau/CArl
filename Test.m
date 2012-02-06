d1fe6cb0f9c48e862fc603cf3c1047769a2614c4
cottereau <regis.cottereau@ecp.fr>
2012-02-06 01:49:06 +0100

    modification of plots in Tests

--- Test.m
+++ Test.m
@@ -53,6 +53,13 @@ end
 % FUNCTION PLOTTEST 1D
 function plottest1D( model, sol )
 figure; plot(model{1}.mesh.X,sol{1}, 'bx-', model{2}.mesh.X,sol{2},'ro--')
+X1 = model{1}.mesh.X;
+s1 = diff( sol{1} ) ./ diff(X1); s1 = [s1 s1]'; s1 = s1(:);
+x1 = [X1(1:(end-1)) X1(2:end)]'; x1 = x1(:);
+X2 = model{2}.mesh.X;
+s2 = diff( sol{2} ) ./ diff(X2); s2 = [s2 s2]'; s2 = s2(:);
+x2 = [X2(1:(end-1)) X2(2:end)]'; x2 = x2(:);
+figure; plot( x1, s1, 'bx-', x2, s2, 'ro--' );
 
 % FUNCTION PLOTTEST 2D
 function plottest2D( model, sol )
@@ -65,16 +72,18 @@ trisurf( model{2}.mesh.T, model{2}.mesh.X(:,1), model{2}.mesh.X(:,2), sol{2});
 % FUNCTION PLOTTESTSTOCHASTIC
 function plotteststochastic( model, sol, out )
 X1 = model{1}.mesh.X;
-s1 = diff( sol{1} ) / mean(diff(X1)); s1 = [s1 s1]'; s1 = s1(:);
+s1 = diff( sol{1} ) ./ diff(X1); s1 = [s1 s1]'; s1 = s1(:);
 x1 = [X1(1:(end-1)) X1(2:end)]'; x1 = x1(:);
 X2 = model{2}.mesh.X;
 x2 = [X2(1:(end-1)) X2(2:end)]'; x2 = x2(:);
 s2 = zeros( length(x2), size(out.u.MC.u,2));
-s2(1:2:end,:) = diff( out.u.MC.u(1:length(X2),:), 1, 1 ) / mean(diff(X2));
+s2(1:2:end,:) = diff( out.u.MC.u(1:length(X2),:), 1, 1 ) ./ ...
+          repmat(diff(X2),[1 size(out.u.MC.u,2)]);
 s2(2:2:end,:) = s2(1:2:end,:);
 ms2 = mean(s2,2);
 ss2 = std(s2,[],2);
 pms2 = [ ms2+ss2/sqrt(1-0.9); ms2(end:-1:1)-ss2(end:-1:1)/sqrt(1-0.9) ];
 figure; fill( [x2;x2(end:-1:1)], pms2, 'y' )
-hold on; plot( x1, s1, 'bx-', x2, ms2, 'ro--' );
+hold on; 
+plot( x1, s1, 'bx-', x2, ms2, 'ro--' );
 
