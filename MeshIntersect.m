d526360a5fb18b88d74954e56eb56e435f5ea052
cottereau <regis.cottereau@ecp.fr>
2012-02-23 12:14:00 +0100

    getting rid of unused geometry functions

--- MeshIntersect.m
+++ MeshIntersect.m
@@ -141,3 +141,20 @@ Mx = indx(:);
 My = repmat( (1:size(Mval,1))', [3 1] );
 Mval = Mval(:);
 
+%==========================================================================
+function [ X, T, Xn ] = ReduceMesh( X, T )
+% REDUCEMESH to get rid of unused nodes and renumber the connectivity
+% matrix
+%
+% syntax: [ X, T, Xn ] = ReduceMesh( X, T )
+%
+%  T,X: connectivity matrices and nodal coordinates [Ne*3 matrix] and 
+%       [Nn*2 matrix]
+%  Xn: indices into the input connectivity matrix
+Xn = unique( T(:) );
+Nn = size(Xn,1);
+for i1 = 1:Nn
+    T( T(:)==Xn(i1) ) = i1;
+end
+X = X(Xn,:);
+
