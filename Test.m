4c05aa237d16e76a3b03bee957288a3d54b2d5d9
leguennec <yleguennec@gmail.com>
2013-03-08 10:10:58 +0100

    small modification in the plot for stochastic-determinist coupling

--- Test.m
+++ Test.m
@@ -3,25 +3,25 @@ function Test(type)
 % The existing tests are the following
 %
 % ONE-DIMENSIONAL TESTS
-%  'zoom1D' for a zoom case in 1D (deterministic continuum-continuum)
-%  'join1D' for a junction case in 1D (deterministic continuum-continuum)
-%  'force1D'
-%  'MC1D' for a joint case in 1D (stochastic continuum-continuum)
-%  'MC1Dstosto' for a joint case in 1D (stochastic-stochastic continuum)
-%  'MC1D_BC_u10'
-%  'MC1Dstosto2' for a joint case in 1D (stochastic-stochastic coupling)
-%  'zoom1Dstosto' for a zoom case in 1D (stochastic-stochastic coupling)
+% 'zoom1D' for a zoom case in 1D (deterministic continuum-continuum)
+% 'join1D' for a junction case in 1D (deterministic continuum-continuum)
+% 'force1D'
+% 'MC1D' for a joint case in 1D (stochastic continuum-continuum)
+% 'MC1Dstosto' for a joint case in 1D (stochastic-stochastic continuum)
+% 'MC1D_BC_u10'
+% 'MC1Dstosto2' for a joint case in 1D (stochastic-stochastic coupling)
+% 'zoom1Dstosto' for a zoom case in 1D (stochastic-stochastic coupling)
 %
 % TWO-DIMENSIONAL TESTS
-%  'join2D' for  a junction case in 2D (deterministic continuum-continuum)
-%  'zoom2D'
-%  'force2D'
-%  'comsol2D'
-%  'nonembedded2D_1'
+% 'join2D' for a junction case in 2D (deterministic continuum-continuum)
+% 'zoom2D'
+% 'force2D'
+% 'comsol2D'
+% 'nonembedded2D_1'
 %
 % the possible series are
-%  'short' for only the short tests
-%  'comsol' for the comsol tests
+% 'short' for only the short tests
+% 'comsol' for the comsol tests
 
 % R. Cottereau 05/2010
 % TO DO
@@ -114,30 +114,6 @@ view(3)
 
 
 % FUNCTION PLOTTESTSTOCHASTIC
-function plotteststochastic( model, sol, out, ref )
-pc = 0.9;
-x1 = model{1}.mesh.X( model{1}.mesh.T )';
-x2 = model{2}.mesh.X( model{2}.mesh.T )';
-dx2 = diff(x2);
-u1 = sol{1}';
-s1 = diff(u1) ./ diff(x1); s1 = [s1; s1];
-s2 = squeeze(diff(out.model{2}.auMC,[],2)) ...
-           ./ repmat(dx2',[1 size(out.model{2}.auMC,3)]);
-ms2 = mean(s2,2); ms2 = [ms2 ms2]'; ms2 = ms2(:);
-ss2 = std(s2,[],2); ss2 = [ss2 ss2]'; ss2 = ss2(:);
-pms2 = [ ms2+ss2/sqrt(1-pc); ms2(end:-1:1)-ss2(end:-1:1)/sqrt(1-pc) ];
-xx2 = x2(:); xx2 = [ xx2; xx2(end:-1:1) ];
-figure; fill( xx2, pms2, 'y' )
-hold on;
-plot( x1, s1, 'bx-', x2(:), ms2, 'ro--' );
-if ~isempty(ref)
-    xref = [ref.X(1:(end-1)) ref.X(2:end)]'; xref = xref(:);
-    sref = diff(ref.u) ./ diff(ref.X); sref = [sref; sref];
-    hold on; plot(xref,sref,'k--')
-end
-
-
-
 function plotteststochastic2( model, sol, out, ref )
 pc = 0.9;
 x1 = model{1}.mesh.X( model{1}.mesh.T )';
@@ -147,8 +123,11 @@ dx1 = diff(x1);
 u1=mean(out.model{1}.auMC,3)';uu1=u1(:);
 stdu1=std(out.model{1}.auMC,0,3)';stdu1=stdu1(:);
 probu1=[uu1+stdu1/sqrt(1-pc);(uu1(end:-1:1)-stdu1(end:-1:1)/sqrt(1-pc))];
-xxx1 = [ (x1(:));(x1(end:-1:1))']; 
-figure;fill( xxx1, probu1, 'y' )
+xxx1 = [ (x1(:));(x1(end:-1:1))'];
+figure;
+if (strcmp(out.coupling{1}.mediator.type,'mesomicro'))
+    fill( xxx1, probu1, 'y' )
+end
 hold on
 u1b = (sol{1})';
 u2b=(sol{2})';
@@ -169,7 +148,10 @@ ms1 = mean(s1,2); ms1 = [ms1 ms1]'; ms1 = ms1(:);
 ss1 = std(s1,[],2); ss1 = [ss1 ss1]'; ss1 = ss1(:);
 pms1 = [ ms1+ss1/sqrt(1-pc); ms1(end:-1:1)-ss1(end:-1:1)/sqrt(1-pc) ];
 xx1 = x1(:); xx1 = [ xx1; xx1(end:-1:1) ];
-figure; fill( xx1, pms1, 'y' )
+figure;
+if (strcmp(out.coupling{1}.mediator.type,'mesomicro'))
+fill( xx1, pms1, 'y' )
+end
 hold on;
 s2 = squeeze(diff(out.model{2}.auMC,[],2)) ...
            ./ repmat(dx2',[1 size(out.model{2}.auMC,3)]);
