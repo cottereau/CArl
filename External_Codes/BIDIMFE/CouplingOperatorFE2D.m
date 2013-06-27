e95ad4f4544972a356e5762ba7c9541d500c45b2
leguennec <yleguennec@gmail.com>
2013-06-27 20:57:30 +0200

    revision of 2D-beam matrix construction

--- External_Codes/BIDIMFE/CouplingOperatorFE2D.m
+++ External_Codes/BIDIMFE/CouplingOperatorFE2D.m
@@ -1,41 +1,50 @@
 function [ x, y, C ] = CouplingOperatorFE2D( operator, mesh, opt )
 
-% YLG 06/2013
 
-% constants
 [Nne,ne] = size(mesh.Triangulation);
 
 % computation of MassMatrix
 m = struct( 'mesh', mesh, ...
-            'property', ones(Nne,ne), ...
-            'load', zeros(Nne,ne), ...
-            'BC', [] );
-        elements3 = mesh.Triangulation;
-coordinates = mesh.X;
-[ M ] = mass_matrixP1_2D_elasticity( elements3,coordinates );
-[x,y] = find(M);
-indtemp = find(M);
-C=M(indtemp);
+    'property', ones(Nne,ne), ...
+    'load', zeros(Nne,ne,2), ...
+    'BC', [] );
+
+elements = m.mesh.Triangulation;
+coordinates = m.mesh.X;
+
+[ Mass1,Mass2 ] = mass_coupling_Timo(elements,coordinates);
+[x2,y2] = find(Mass2);
+indtemp = (find(Mass2));
+C = Mass2(indtemp);
+[x,y] = find(Mass1);
+maxx = size(Mass1,1);
+x = [x;x2];
+y = [y;y2+maxx];
+indtemp = (find(Mass1));
+C = [Mass1(indtemp);C];
 % choice of the coupling operator
 switch operator
     
     % L2 coupling
-    case 'L2'            
+    case 'L2'
         
-    % H1 coupling
+        % H1 coupling
     case 'H1'
-lambda=ones([size(elements3,1) 2]); mu=zeros([size(elements3,1) 2]); %Lamme coeficients
-%dirichlet = [model.BC.nodes(1:end-1) model.BC.nodes(2:end)];
-P = zeros([size(elements3,1) size(elements3,2) 2]);
-        [ KK, ~, ~ ] = stifness_matrixP1_2D_elasticity(elements3,coordinates,lambda,mu,P );
-        [z,k] = find(KK);
-        indtemp = find(KK);
-        K = KK(indtemp);
+        [stiffC1,stiffC2] = stifness_coupling_Timo( elements,coordinates);
+        [z2,k2] = find(stiffC2);
+        indtemp= find(stiffC2);
+        K2=(stiffC2(indtemp));
+        x = [ z2; x ];
+        y = [ k2+maxx; y ];
+        C = [ opt.kappa*K2; C ];
+        
+        [z,k] = find(stiffC1);
+        indtemp= find(stiffC1);
+        K=(stiffC1(indtemp));
         x = [ z; x ];
         y = [ k; y ];
         C = [ opt.kappa*K; C ];
-        
-    % unknown coupling operator
+        % unknown coupling operator
     otherwise
         error('unknown coupling operator')
 end
