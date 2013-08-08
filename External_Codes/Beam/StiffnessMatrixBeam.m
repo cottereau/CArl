99851f9af6aecff08cfcc8e8a018dc40289c7894
cottereau <regis.cottereau@ecp.fr>
2013-08-08 12:17:44 -0300

    implemented loading and boundary conditions in Beam

--- External_Codes/Beam/StiffnessMatrixBeam.m
+++ External_Codes/Beam/StiffnessMatrixBeam.m
@@ -20,12 +20,19 @@ function [ K, F ] = StiffnessMatrixBeam( model, alpha )
         warning('varying alpha not implemented yet for beams')
 
 % geometrical parameters
-h = model.L;
-I = h^3/12;
+b = 1;
+h = model.h;
+S = b*h;
+I = b*h^3/12;
+
+% mechanical parameters
 E = model.young;
-AG = h * 1 * model.young/2/(1+model.poisson);
-C = [ E*I 0; 0 AG];
-P = 0; % loading (to be modified in the future)
+G = model.young/2/(1+model.poisson);
+C = [ E*I 0; 0 S*G];
+P = model.P;
+if P~=0
+    warning('pressure variation through alpha not be implemented in beam');
+end
 
 % mesh
 X = model.X;
@@ -38,3 +45,43 @@ Nd = 2*Nn;
 [ K, F ] = formStiffnessMassTimoshenkoBeam(Nd,Ne,T,Nn,X,C,P,1,I,h);
 K = sparse(K);
 F = sparse(F);
+
+% add boundary conditions
+if isfield(model,'BC')&&~isempty( model.BC )
+
+    % Dirichlet Boundary Conditions in displacement
+    ind = find(model.BC.type == 'U');
+    Nbc = length(ind);
+    BC = sparse( model.BC.nodes(ind), 1:Nbc, 1, Nd, Nbc );
+    FBC = sparse( 1:Nbc, 1, model.BC.value(ind), Nbc, 1 );
+
+    % Dirichlet Boundary Conditions in rotation
+    ind = find(model.BC.type == 'R');
+    Nbc = length(ind);
+    BC = [BC sparse( model.BC.nodes(ind)+Nn, 1:Nbc, 1, Nd, Nbc )];
+    FBC = [FBC; sparse( 1:Nbc, 1, model.BC.value(ind), Nbc, 1 )];
+
+    % Neumann Boundary Conditions in Force
+    ind = find(model.BC.type == 'F');
+    Nbc = length(ind);
+    if Nbc>0
+        val = model.BC.value(ind);
+        ind = model.BC.node(ind);
+        F(ind) = F(ind) + sparse( 1:Nbc, 1, val, Nbc, 1 );
+    end
+
+    % Neumann Boundary Conditions in moment
+    ind = find(model.BC.type == 'M');
+    Nbc = length(ind);
+    if Nbc>0
+        val = model.BC.value(ind);
+        ind = model.BC.node(ind) + Nn;
+        F(ind) = F(ind) + sparse( 1:Nbc, 1, val, Nbc, 1 );
+    end
+
+    % construct new matrices
+    K = [ K BC; BC' sparse(size(BC,2),size(BC,2)) ];
+    F = [ F; FBC ]; 
+    
+end
+
