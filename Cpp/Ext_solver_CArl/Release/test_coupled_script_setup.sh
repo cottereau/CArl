78c84d6b6b17212b5107fae6b81a3c0b8f4f777f
ThiagoMS3 <ThiagoMS3@users.noreply.github.com>
2017-05-16 18:59:26 +0200

    [UPDATE] Finished setting up the brick coupled test

--- Cpp/Ext_solver_CArl/Release/test_coupled_script_setup.sh
+++ Cpp/Ext_solver_CArl/Release/test_coupled_script_setup.sh
@@ -1,33 +1,11 @@
 #!/bin/bash
+mkdir -p examples/coupled_traction_test/FETI_solver/brick_traction_1k/system_matrices
+
 echo " -------- Assemble clamped coupled model ..."
-mpirun -n $1 ./libmesh_assemble_lin_homogeneous__min_x_clamped -i examples/coupled_traction_test/FETI_solver/assemble_brick_traction_A_1k.txt > assemble_clamped_coupled_model.log
+mpirun -n $1 ./libmesh_assemble_lin_homogeneous__min_x_clamped -i examples/coupled_traction_test/FETI_solver/brick_traction_1k/assemble_brick_traction_A_1k.txt > assemble_clamped_coupled_model.log
 
 echo " -------- Assemble traction coupled model ..."
-mpirun -n $1 ./libmesh_assemble_lin_homogeneous__max_x_traction -i examples/coupled_traction_test/FETI_solver/assemble_brick_traction_B_1k.txt > assemble_traction_coupled_model.log
+mpirun -n $1 ./libmesh_assemble_lin_homogeneous__max_x_traction -i examples/coupled_traction_test/FETI_solver/brick_traction_1k/assemble_brick_traction_B_1k.txt > assemble_traction_coupled_model.log
 
 echo " -------- Assemble coupling matrices ..."
-mpirun -n $1 ./CArl_assemble_coupling -i examples/coupled_traction_test/FETI_solver/assemble_coupling_1k.txt > assemble_coupling_matrices.log
-
-echo " -------- FETI setup - initialize ..."
-mpirun -n $1 ./CArl_FETI_setup_init -i examples/coupled_traction_test/FETI_solver/setup_FETI_solver_1k.txt
-
-echo " -------- Solve model A - uncoupled ..."
-mpirun -n $1 ./libmesh_solve_linear_system -i examples/coupled_traction_test/FETI_solver/scratch_folder/ext_solver_u0_A.txt
-
-echo " -------- Solve model B - uncoupled ..."
-mpirun -n $1 ./libmesh_solve_linear_system -i examples/coupled_traction_test/FETI_solver/scratch_folder/ext_solver_u0_B.txt
-
-echo " -------- Solve model A - initialize ..."
-mpirun -n $1 ./libmesh_solve_linear_system -i examples/coupled_traction_test/FETI_solver/scratch_folder/ext_solver_A.txt
-
-echo " -------- Solve model B - initialize ..."
-mpirun -n $1 ./libmesh_solve_linear_system -i examples/coupled_traction_test/FETI_solver/scratch_folder/ext_solver_B.txt
-
-echo " -------- FETI setup - finalize ..."
-mpirun -n $1 ./CArl_FETI_setup_finish -i examples/coupled_traction_test/FETI_solver/scratch_folder/CArl_FETI_setup_finish.txt
-
-echo " -------- Solve model A - iter. no. 0 ..."
-mpirun -n $1 ./libmesh_solve_linear_system -i examples/coupled_traction_test/FETI_solver/scratch_folder/ext_solver_A.txt
-
-echo " -------- Solve model B - iter. no. 0 ..."
-mpirun -n $1 ./libmesh_solve_linear_system -i examples/coupled_traction_test/FETI_solver/scratch_folder/ext_solver_B.txt
+mpirun -n $1 ./CArl_assemble_coupling -i examples/coupled_traction_test/FETI_solver/brick_traction_1k/assemble_coupling_1k.txt > assemble_coupling_matrices.log
