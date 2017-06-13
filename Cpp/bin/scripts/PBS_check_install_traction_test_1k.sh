81848a00960a9d8790583342c8a3fca8df11a1d2
ThiagoMS3 <thiagoms3@gmail.com>
2017-06-13 21:00:07 +0200

    [BUG] Corrected the PBS check script

--- Cpp/bin/scripts/PBS_check_install_traction_test_1k.sh
+++ Cpp/bin/scripts/PBS_check_install_traction_test_1k.sh
@@ -2,16 +2,17 @@
 # NOTE : remember to set the modules and any other PBS options before launching
 
 # - Build the intersections
-job_inter = qsub scripts/PBS_FETI_test_inter_traction_test_1k.pbs
+job_inter=`qsub scripts/PBS_FETI_test_inter_traction_test_1k.pbs`
 
 # - Prepare the external solver
-job_ext   = qsub scripts/PBS_FETI_test_assemble_ext_solver_traction_test_1k.pbs
+job_ext=`qsub scripts/PBS_FETI_test_assemble_ext_solver_traction_test_1k.pbs`
 
 # - Build the coupling matrices AFTER finishing $job_inter
-job_coupl = qsub -W depend=afterok:$job_inter scripts/PBS_FETI_test_assemble_coupling_matrices_traction_test_1k.pbs
+job_coupl=`qsub -W depend=afterok:$job_inter scripts/PBS_FETI_test_assemble_coupling_matrices_traction_test_1k.pbs`
 
 # - Solve the coupled system AFTER finishing both $job_ext and $job_coupl
-job_solve = qsub -W depend=afterok:$job_ext:$job_coupl PBS_FETI_launch_coupled_solver.pbs
+job_solve=`qsub -W depend=afterok:$job_ext:$job_coupl scripts/PBS_FETI_launch_coupled_solver.pbs`
 
 # - Set the coupled solution mesh AFTER finishing $job_solve
-job_treat = qsub -W depend=afterok:$job_solve PBS_FETI_apply_solution_traction_test_1k.pbs
+echo " ---> After all the solver jobs are finished, execute:"
+echo "      qsub scripts/PBS_FETI_apply_solution_traction_test_1k.pbs"
