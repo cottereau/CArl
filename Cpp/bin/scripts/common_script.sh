5c238dcc09df28ec27555eba39df75a4d1465bde
Regis Cottereau <cottereau@fusion.ib0.ice.centralesupelec.fr>
2019-05-21 00:34:59 +0200

    [UPDATE] default fusion modules and options

--- Cpp/bin/scripts/common_script.sh
+++ Cpp/bin/scripts/common_script.sh
@@ -2,13 +2,15 @@
 #PBS -l select=1:ncpus=4:mpiprocs=4
 
 # "fusion" PBS options
-# #PBS -q haswellq
 # #PBS -P [PROJECT NAME]
 
 # Charge the modules here
 # "fusion" cluster modules
 # module purge
-# module load intel-compilers/16.0.3
-# module load intel-mpi/5.1.2
+# module load intel-compilers/17.0.4
+# module load intel-mpi/2017.0.3
+# module load intel-mkl/2017.0.3
+# module load libgmp/6.1.2
+# module load libmpfr/3.1.5
 
 cd $PBS_O_WORKDIR
