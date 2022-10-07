27be0764c72fcd70bbd6a84bd683edca3eedcdba
Chensheng Luo <chensheng-sofiane.luo@student-cs.fr>
2022-10-08 01:42:45 +0200

    delete traction_test(already set to static_traction_test)

--- Cpp/examples/coupled_traction_test/scripts/LOCAL_FETI_test_assemble_ext_solver_traction_test_1k.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p ../examples/coupled_traction_test/FETI_solver/brick_traction_1k/system_matrices
-
-echo " --- Assemble clamped coupled model ..."
-mpirun -n 4 ./libmesh_assemble_lin_homogeneous__min_x_clamped -i ../examples/coupled_traction_test/FETI_solver/brick_traction_1k/assemble_brick_traction_A_1k.txt > assemble_clamped_coupled_model.log
-
-echo " --- Assemble traction coupled model ..."
-mpirun -n 4 ./libmesh_assemble_lin_homogeneous__max_x_traction -i ../examples/coupled_traction_test/FETI_solver/brick_traction_1k/assemble_brick_traction_B_1k.txt > assemble_traction_coupled_model.log
--- Cpp/examples/coupled_traction_test/scripts/LOCAL_test_single_model_traction_test_1k.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo " --- Assemble single model traction ..."
-mpirun -n 4 ./libmesh_assemble_lin_homogeneous__traction_test -i $CARLBUILD/examples/single_traction_test/assemble_traction_test_1k.txt > assemble_single_model.log
-
-echo " --- Solve single model traction ..."
-mpirun -n 4 ./libmesh_solve_linear_system -i $CARLBUILD/examples/single_traction_test/solve_traction_test_1k.txt > solve_single_model.log
